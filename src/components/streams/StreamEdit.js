799524a021ea447dcf98b85798200dea7ddf9d5f
Venkatesh <VenkateshwaranMarichamy@gmail.com>
2022-08-13 00:43:03 +0530

    REST based React apps completed

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,20 +1,34 @@
+import _ from 'lodash';
 import React from "react";
 import { connect } from "react-redux";
-import { fetchStream } from "../../actions"
+import { formValues } from "redux-form";
+import { fetchStream, editStream } from "../../actions";
+import StreamForm from "./StreamForm";
 
 class StreamEdit extends React.Component {
 
     componentDidMount() {
         this.props.fetchStream(this.props.match.params.id);
-    }
+    };
+
+    onSubmit = formValues => {
+        //console.log(formValues)
+        this.props.editStream(this.props.match.params.id, formValues);
+    };
 
     render() {
-       // console.log(this.props);
+        // console.log(this.props);
 
-       if(!(this.props.stream)){
-        return <div>Loading...</div>;
-       }
-        return <div>{this.props.stream.title}</div>;
+        if (!(this.props.stream)) {
+            return <div>Loading...</div>;
+        }
+        return (
+            <div>
+                <h3>Edit a Stream</h3>
+                {/* initial values will map to Field comp
+                <StreamForm initialValues={{title: this.props.stream.title, description: this.props.stream.description}} onSubmit={this.onSubmit} /> */}
+                <StreamForm initialValues={_.pick(this.props.stream,'title','description')} onSubmit={this.onSubmit} />
+            </div>);
     }
 }
 
@@ -24,4 +38,4 @@ const mapStateToProps = (state, ownProps) => {
     return { stream: state.streams[ownProps.match.params.id] };
 }
 
-export default connect(mapStateToProps, { fetchStream })(StreamEdit);
+export default connect(mapStateToProps, { fetchStream, editStream })(StreamEdit);
